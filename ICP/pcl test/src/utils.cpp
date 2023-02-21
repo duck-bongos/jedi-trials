a25266234230b8b80ed6afbd344f1804a9d970af
Dan Billmann <billmannd@gmail.com>
2023-02-21 13:15:25 -0500

    more cleanup

--- ICP/pcl test/src/utils.cpp
+++ ICP/pcl test/src/utils.cpp
@@ -4,8 +4,8 @@
 #include "read_config.h"
 
 /** global variable definition**/
-std::string g_DATASET_DIR;	// Dataset directory
-std::string g_RESULT_DIR;	// Result directory
+std::string g_DATASET_DIR; // Dataset directory
+std::string g_RESULT_DIR;  // Result directory
 
 // Normal Estimation Parameters
 float g_NORMAL_SEARCH_RADIUS;
@@ -77,7 +77,7 @@ void readConfigFile(std::string file_name)
 	g_ICP_MAX_ITERATION = config_file.Read("ICP_Max_Iteration", g_ICP_MAX_ITERATION);
 	g_ICP_USE_RANSAC = config_file.Read("ICP_Use_Ransac", g_ICP_USE_RANSAC);
 	g_ICP_USE_RECIPROCAL_SEARCH = config_file.Read("ICP_Use_Reciprocal_Search", g_ICP_USE_RECIPROCAL_SEARCH);
-	
+
 	g_ICP_TRANSFORMATION_EPSILON = config_file.Read("ICP_Transformation_Epsilon", g_ICP_TRANSFORMATION_EPSILON);
 	g_ICP_MAX_CORRESPONDENCE_DISTANCE = config_file.Read("ICP_Max_Correspondence_Distance", g_ICP_MAX_CORRESPONDENCE_DISTANCE);
 	g_ICP_EUCLIDEAN_FITNESS_EPSILON = config_file.Read("ICP_Euclidean_Fitness_Epsilon", g_ICP_EUCLIDEAN_FITNESS_EPSILON);
@@ -92,7 +92,8 @@ void readConfigFile(std::string file_name)
 
 	g_SAVE_APPROX_DATA = config_file.Read("Save_Approx_Data", g_SAVE_APPROX_DATA);
 
-	std::cout << std::endl << "Reading Config Successfully!" << std::endl;
+	std::cout << std::endl
+			  << "Reading Config Successfully!" << std::endl;
 }
 
 void makeResultFolder(std::string &g_result_dir)
@@ -105,7 +106,7 @@ void makeResultFolder(std::string &g_result_dir)
 	std::vector<std::string> file_name;
 
 	cmd = "mkdir -p " + g_result_dir;
-        system(cmd.c_str());
+	system(cmd.c_str());
 
 	dirp = opendir(g_result_dir.c_str());
 	if (dirp)
@@ -119,7 +120,7 @@ void makeResultFolder(std::string &g_result_dir)
 
 		if (file_name.size() == 2)
 		{
-			cmd = "mkdir " + g_result_dir+ "result_1";
+			cmd = "mkdir " + g_result_dir + "result_1";
 		}
 		else
 		{
@@ -132,7 +133,6 @@ void makeResultFolder(std::string &g_result_dir)
 					if (last_file_id < cur_file_id)
 						last_file_id = cur_file_id;
 				}
-
 			}
 			cmd = "mkdir " + g_result_dir + "result_" + std::to_string(last_file_id + 1);
 		}
@@ -147,22 +147,23 @@ void makeResultFolder(std::string &g_result_dir)
 			exit(-1);
 		}
 	}
-	else {
+	else
+	{
 		std::cout << errno << std::endl;
 	}
 }
 
-bool sortString (std::string i,std::string j) 
+bool sortString(std::string i, std::string j)
 {
-	// the part before '.bin' 
-	std::string foo_i = i.substr(0,i.length()-4);
-	std::string foo_j = j.substr(0,i.length()-4);
-	return (foo_i<foo_j); 
+	// the part before '.bin'
+	std::string foo_i = i.substr(0, i.length() - 4);
+	std::string foo_j = j.substr(0, i.length() - 4);
+	return (foo_i < foo_j);
 }
 
 void listDir(std::string path_dir, std::vector<std::string> &files)
 {
-	DIR* dirp = opendir(path_dir.c_str());
+	DIR *dirp = opendir(path_dir.c_str());
 
 	int file_num = 0;
 	if (!dirp)
@@ -171,8 +172,9 @@ void listDir(std::string path_dir, std::vector<std::string> &files)
 		return;
 	}
 
-	struct dirent * dp;
-	while ((dp = readdir(dirp)) != NULL) {
+	struct dirent *dp;
+	while ((dp = readdir(dirp)) != NULL)
+	{
 
 		std::string str_d_name(dp->d_name);
 
@@ -188,10 +190,11 @@ void listDir(std::string path_dir, std::vector<std::string> &files)
 			files.push_back(path_dir + '/' + str_d_name);
 	}
 	closedir(dirp);
-	std::cout << "total number of frames: " << file_num << std::endl << std::endl;
+	std::cout << "total number of frames: " << file_num << std::endl
+			  << std::endl;
 
 	// sort
-	std::sort (files.begin(), files.end(), sortString);
+	std::sort(files.begin(), files.end(), sortString);
 }
 
 void loadBINFile(std::string infile, FeatureCloud &cloud)
@@ -201,21 +204,23 @@ void loadBINFile(std::string infile, FeatureCloud &cloud)
 
 	std::fstream input(infile.c_str(), std::ios::in | std::ios::binary);
 
-	if(!input.good()){
+	if (!input.good())
+	{
 		std::cerr << "Could not read file: " << infile << std::endl;
 		exit(EXIT_FAILURE);
 	}
 	input.seekg(0, std::ios::beg);
 
 	/* convertion */
-	PointCloud::Ptr points (new PointCloud);
+	PointCloud::Ptr points(new PointCloud);
 
-	for (int j=0; input.good() && !input.eof(); j++) {
-		//PointXYZI point;
+	for (int j = 0; input.good() && !input.eof(); j++)
+	{
+		// PointXYZI point;
 		PointT point, point_gt;
 
-		input.read((char *) &point.x, 3*sizeof(float));
-		input.seekg(sizeof(float), std::ios::cur);    
+		input.read((char *)&point.x, 3 * sizeof(float));
+		input.seekg(sizeof(float), std::ios::cur);
 
 		/* coordinate system trans*/
 		point_gt.x = -point.y;
@@ -228,64 +233,69 @@ void loadBINFile(std::string infile, FeatureCloud &cloud)
 	cloud.setInputCloud(points);
 	input.close();
 
-	std::cout << "Number Of Points Loaded: " << points->points.size() << std::endl << std::endl;
+	std::cout << "Number Of Points Loaded: " << points->points.size() << std::endl
+			  << std::endl;
 }
 
 void loadTXTFile(std::string infile, FeatureCloud &cloud)
 {
-    std::cout << "Loading " << infile << std::endl;
+	std::cout << "Loading " << infile << std::endl;
 
-    std::ifstream file(infile.c_str());
-    std::string str;
+	std::ifstream file(infile.c_str());
+	std::string str;
 
-    PointCloud::Ptr points (new PointCloud);
-    std::vector<std::string> raw_data;
+	PointCloud::Ptr points(new PointCloud);
+	std::vector<std::string> raw_data;
 
-    while (std::getline(file, str))
-        raw_data.push_back(str);
-    std:: cout << "Size: " << raw_data.size() << std::endl;
+	while (std::getline(file, str))
+		raw_data.push_back(str);
+	std::cout << "Size: " << raw_data.size() << std::endl;
 
-    for (int i = 0; i < raw_data.size(); i++)
-    {
-        PointT point;
+	for (int i = 0; i < raw_data.size(); i++)
+	{
+		PointT point;
 
-        std::stringstream raw_ss(raw_data[i]);
+		std::stringstream raw_ss(raw_data[i]);
 
-        int j = 0;
-        while (raw_ss.good())
-        {
-           std::string substr;
-           std::getline(raw_ss, substr, ',');
-           float value_ = std::stof(substr);
-           if (j % 3 == 0) point.x = value_;
-           if (j % 3 == 1) point.y = value_;
-           if (j % 3 == 2) point.z = value_;
-           j += 1;
-        }
+		int j = 0;
+		while (raw_ss.good())
+		{
+			std::string substr;
+			std::getline(raw_ss, substr, ',');
+			float value_ = std::stof(substr);
+			if (j % 3 == 0)
+				point.x = value_;
+			if (j % 3 == 1)
+				point.y = value_;
+			if (j % 3 == 2)
+				point.z = value_;
+			j += 1;
+		}
 
-        points->push_back(point);
-    }
+		points->push_back(point);
+	}
 
-    cloud.setInputCloud(points);
-    std::cout << "Number Of Points Loaded: " << points->points.size() << std::endl << std::endl;
+	cloud.setInputCloud(points);
+	std::cout << "Number Of Points Loaded: " << points->points.size() << std::endl
+			  << std::endl;
 }
 
 void getIndices(FeatureCloud &cloud)
 {
 	const PointCloud::Ptr cloudin_ptr = cloud.getPointCloud();
 	const PointCloud::Ptr keypoints_cal_ptr = cloud.getKeyPoints();
-	const PointCloud::Ptr keypoints_real_ptr (new PointCloud);
-	pcl::PointIndices::Ptr indices_ptr (new pcl::PointIndices);
+	const PointCloud::Ptr keypoints_real_ptr(new PointCloud);
+	pcl::PointIndices::Ptr indices_ptr(new pcl::PointIndices);
 
 	pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
 	kdtree.setInputCloud(cloudin_ptr);
-	std::vector<float>pointNKNSquareDistance;
+	std::vector<float> pointNKNSquareDistance;
 	std::vector<int> pointIdxNKNSearch;
 
 	int indice = 0;
-	for (size_t i =0; i < keypoints_cal_ptr->size();i++)
+	for (size_t i = 0; i < keypoints_cal_ptr->size(); i++)
 	{
-		kdtree.nearestKSearch(keypoints_cal_ptr->points[i],1,pointIdxNKNSearch,pointNKNSquareDistance);
+		kdtree.nearestKSearch(keypoints_cal_ptr->points[i], 1, pointIdxNKNSearch, pointNKNSquareDistance);
 
 		indice = pointIdxNKNSearch[0];
 		indices_ptr->indices.push_back(indice);
@@ -298,13 +308,13 @@ void getIndices(FeatureCloud &cloud)
 
 void writeMatrix(std::ofstream &stream_name, std::string file_name, Eigen::Matrix4f &stream_data)
 {
-	stream_name.open (file_name, std::fstream::app);
+	stream_name.open(file_name, std::fstream::app);
 
-	for(int i = 0; i < 3; i ++)
+	for (int i = 0; i < 3; i++)
 	{
-		for(int j = 0; j < 4; j ++)
+		for (int j = 0; j < 4; j++)
 		{
-			stream_name << ("%lf", stream_data(i,j));
+			stream_name << ("%lf", stream_data(i, j));
 			if (j != 4)
 				stream_name << " ";
 		}
@@ -316,11 +326,11 @@ void writeMatrix(std::ofstream &stream_name, std::string file_name, Eigen::Matri
 void printMatrix(std::string matrix_name, Eigen::Matrix4f &matrix)
 {
 	std::cout << matrix_name << ": " << std::endl;
-	for(int i = 0; i < 3; i ++)
+	for (int i = 0; i < 3; i++)
 	{
-		for(int j = 0; j < 4; j ++)
+		for (int j = 0; j < 4; j++)
 		{
-			std::cout << ("%lf", matrix(i,j));
+			std::cout << ("%lf", matrix(i, j));
 			if (j != 4)
 				std::cout << " ";
 		}
@@ -348,7 +358,7 @@ void detectKeyPoints(FeatureCloud &cloud)
 	{
 		std::cout << "HARRIS Keypoints Are Being Detected!" << std::endl;
 		keyPointsHARRIS(cloud);
-	}	
+	}
 
 	getIndices(cloud);
 }
@@ -358,13 +368,13 @@ void describeFeatures(FeatureCloud &cloud)
 	if (g_FEATURE_MODULE == "FPFH")
 	{
 		std::cout << "FPFH Descriptors Are Being Detected!" << std::endl;
-		computeFeatures_FPFH (cloud, g_FEATURE_SEARCH_RADIUS);
+		computeFeatures_FPFH(cloud, g_FEATURE_SEARCH_RADIUS);
 	}
 
 	if (g_FEATURE_MODULE == "SHOT")
 	{
 		std::cout << "SHOT Descriptors Are Being Detected!" << std::endl;
-		computeFeatures_SHOT (cloud, g_FEATURE_SEARCH_RADIUS);
+		computeFeatures_SHOT(cloud, g_FEATURE_SEARCH_RADIUS);
 	}
 }
 
@@ -383,22 +393,22 @@ void removeNANFromNormal(FeatureCloud &cloud)
 	cloud.setInputCloud(nanremoved_);
 
 	std::cout << "Remove NAN From Normals:" << std::endl;
-	std::cout << (num_normal - normals_->size()) << " Normals Are Removed: " \
-	<< num_normal << "->" << normals_->size() << std::endl;
-	std::cout << (num_point - nanremoved_->size()) << " Points Are Removed: " \
-	<< num_point << "->" << nanremoved_->size() << std::endl << std::endl;
-
+	std::cout << (num_normal - normals_->size()) << " Normals Are Removed: "
+			  << num_normal << "->" << normals_->size() << std::endl;
+	std::cout << (num_point - nanremoved_->size()) << " Points Are Removed: "
+			  << num_point << "->" << nanremoved_->size() << std::endl
+			  << std::endl;
 }
 
 void removeNANFromFPFH(FPFH_Features::Ptr feature_descriptor, FPFH_Features::Ptr nanremoved, FeatureCloud &cloud)
 {
 	PointCloud::Ptr keyPoints_nanremoved_ptr(new PointCloud);
-	pcl::PointIndices::Ptr indices_nanremoved_ptr (new pcl::PointIndices);
+	pcl::PointIndices::Ptr indices_nanremoved_ptr(new pcl::PointIndices);
 
-	for (int i=0; i<feature_descriptor->points.size();i++)
+	for (int i = 0; i < feature_descriptor->points.size(); i++)
 	{
 		float p = feature_descriptor->points[i].histogram[0];
-		if (p != p) 
+		if (p != p)
 		{
 			continue;
 		}
@@ -409,13 +419,14 @@ void removeNANFromFPFH(FPFH_Features::Ptr feature_descriptor, FPFH_Features::Ptr
 			indices_nanremoved_ptr->indices.push_back((cloud.getKeyPoint_indices())->indices[i]);
 		}
 	}
-	if(feature_descriptor->points.size() != nanremoved->points.size())
+	if (feature_descriptor->points.size() != nanremoved->points.size())
 	{
 		cloud.setKeyPoints(keyPoints_nanremoved_ptr);
 		cloud.setKeyPoint_indices(indices_nanremoved_ptr);
 	}
 
 	std::cout << "Remove NAN From Feature Descriptors:" << std::endl;
-	std::cout << (feature_descriptor->points.size() - nanremoved->points.size()) << " Feature Descriptors Are Removed: " \
-	<< feature_descriptor->points.size() << "->" << nanremoved->points.size() << std::endl << std::endl;
+	std::cout << (feature_descriptor->points.size() - nanremoved->points.size()) << " Feature Descriptors Are Removed: "
+			  << feature_descriptor->points.size() << "->" << nanremoved->points.size() << std::endl
+			  << std::endl;
 }
