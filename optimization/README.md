a783460c19b25f5849ee2ddf93c04fbd936eb705
Dan Billmann <billmannd@gmail.com>
2023-03-23 14:00:45 -0400

    Reorganized the directory, removing ICP, registration.
    may remove some of the harmonicmap/3rdparty directories in the future

---
+++ optimization/README.md
@@ -0,0 +1,5 @@
+# PLACEHOLDER
+
+## Optimization 
+A README detailing the types of optimization code you can run from this library
+
--- registration/src/utils.cc
+++
@@ -1,8 +0,0 @@
-#include "mediapipe/modules/face_mesh_calculator.pb.h"
-#include "mediapipe/modules/face_mesh/face_mesh_cpu.pb.h"
-
-int main()
-{
-    std::cout << "WHAT" << std::endl;
-    return 0;
-}
