902482d0c9a403bf866c0dcaed3010d43c2934e7
Dan Billmann <billmannd@gmail.com>
2023-03-26 01:13:59 -0400

    got it to run on mac, brought it to mac branch
    Idk why running it in debug mode helps buttttt it did!

--- optimization/HarmonicMap/harmonic_map/src/main.cc
+++ optimization/HarmonicMap/harmonic_map/src/main.cc
@@ -1,7 +1,7 @@
-#include<math.h>
-#include<iostream>
-#include<stdio.h>
-#include<stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "HarmonicMap.h"
 #include "HarmonicMapMesh.h"
@@ -12,23 +12,22 @@ using namespace MeshLib;
 CHarmonicMapMesh g_mesh;
 CHarmonicMap g_mapper;
 
-
 /*! Normalize g_mesh
  * \param pMesh the input g_mesh
  */
-void normalizeMesh(CHarmonicMapMesh* pMesh)
+void normalizeMesh(CHarmonicMapMesh *pMesh)
 {
     CPoint s(0, 0, 0);
     for (CHarmonicMapMesh::MeshVertexIterator viter(pMesh); !viter.end(); ++viter)
     {
-        CHarmonicMapVertex* v = *viter;
+        CHarmonicMapVertex *v = *viter;
         s = s + v->point();
     }
     s = s / pMesh->numVertices();
 
     for (CHarmonicMapMesh::MeshVertexIterator viter(pMesh); !viter.end(); ++viter)
     {
-        CHarmonicMapVertex* v = *viter;
+        CHarmonicMapVertex *v = *viter;
         CPoint p = v->point();
         p = p - s;
         v->point() = p;
@@ -37,7 +36,7 @@ void normalizeMesh(CHarmonicMapMesh* pMesh)
     double d = 0;
     for (CHarmonicMapMesh::MeshVertexIterator viter(pMesh); !viter.end(); ++viter)
     {
-        CHarmonicMapVertex* v = *viter;
+        CHarmonicMapVertex *v = *viter;
         CPoint p = v->point();
         for (int k = 0; k < 3; k++)
         {
@@ -47,29 +46,28 @@ void normalizeMesh(CHarmonicMapMesh* pMesh)
 
     for (CHarmonicMapMesh::MeshVertexIterator viter(pMesh); !viter.end(); ++viter)
     {
-        CHarmonicMapVertex* v = *viter;
+        CHarmonicMapVertex *v = *viter;
         CPoint p = v->point();
         p = p / d;
         v->point() = p;
     }
 };
 
-
 /*! Compute the face normal and vertex normal
  * \param pMesh the input g_mesh
  */
-void computeNormal(CHarmonicMapMesh* pMesh)
+void computeNormal(CHarmonicMapMesh *pMesh)
 {
     for (CHarmonicMapMesh::MeshVertexIterator viter(pMesh); !viter.end(); ++viter)
     {
-        CHarmonicMapVertex* v = *viter;
+        CHarmonicMapVertex *v = *viter;
         CPoint n(0, 0, 0);
         for (CHarmonicMapMesh::VertexFaceIterator vfiter(v); !vfiter.end(); ++vfiter)
         {
-            CHarmonicMapFace* pF = *vfiter;
+            CHarmonicMapFace *pF = *vfiter;
 
             CPoint p[3];
-            CHalfEdge* he = pF->halfedge();
+            CHalfEdge *he = pF->halfedge();
             for (int k = 0; k < 3; k++)
             {
                 p[k] = he->target()->point();
@@ -86,43 +84,44 @@ void computeNormal(CHarmonicMapMesh* pMesh)
     }
 };
 
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage:\n%s input.m\n--or--\n%s input.obj", argv[0], argv[0]);
+        return EXIT_FAILURE;
+    }
 
-int main(int argc, char * argv[]) {
-	if (argc < 2)
-	{
-		printf("Usage:\n%s input.m\n--or--\n%s input.obj", argv[0], argv[0]);
-		return EXIT_FAILURE;
-	}
+    // Read in the mesh from the file
+    std::string mesh_name(argv[1]);
 
-	// Read in the mesh from the file
-	std::string mesh_name(argv[1]);
+    if (strutil::endsWith(mesh_name, ".m"))
+    {
+        g_mesh.read_m(mesh_name.c_str());
+    }
+    else if (strutil::endsWith(mesh_name, ".obj"))
+    {
+        g_mesh.read_obj(mesh_name.c_str());
+    }
+    else
+    {
+        printf("Only file extensions '.m' and '.obj' supported.\n");
+        return EXIT_FAILURE;
+    }
 
-	if (strutil::endsWith(mesh_name, ".m"))
-	{
-		g_mesh.read_m(mesh_name.c_str());
-	}
-	else if (strutil::endsWith(mesh_name, ".obj")) 
-	{
-		g_mesh.read_obj(mesh_name.c_str());
-	}
-	else
-	{
-		printf("Only file extensions '.m' and '.obj' supported.\n");
-		return EXIT_FAILURE;
-	}
-
-	// prepare the mesh for computation
-	normalizeMesh(&g_mesh);
+    // prepare the mesh for computation
+    normalizeMesh(&g_mesh);
     computeNormal(&g_mesh);
     g_mapper.set_mesh(&g_mesh);
-    
+
     /*Compute the Harmonic Map*/
-    g_mapper.iterative_map();
+    g_mapper.map();
+    g_mesh.write_obj("/Users/dan/Dropbox/SBU/spring_2023/thesis/jedi-trials/data/annotated/working_objects");
+    printf("Wrote to data/annotated/working_objects.");
 
     /*!
      * Write the computed results to a file
      * */
     g_mesh.write_obj("/Users/dan/Desktop/flat_dan.obj");
     return EXIT_SUCCESS;
-
 }
