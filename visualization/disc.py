2f380d89c878dd3d2e38ea6fb1d0c5e3ea6115aa
Dan Billmann <billmannd@gmail.com>
2023-03-27 16:41:04 -0400

    minor visualization update

--- visualization/disc.py
+++ visualization/disc.py
@@ -1,4 +1,4 @@
-"""A module to read in an .obj file and write the textures to an image
+"""A module to read in an .obj file and write the textures to an image.
 
 	Author: Dan Billmann
 
@@ -9,9 +9,29 @@ import sys
 import numpy as np
 
 
+def extract(readlist, numeric=False) -> list:
+    readlist = [r.split(" ")[1:] for r in readlist]
 
-if __name__ in '__main__':
-	with open(sys.argv[1], 'r') as o:
-		obj = o.read()
+    if numeric:
+        extracted = np.array(readlist).astype(float)
 
-	re.findall()
+    else:
+        extracted = np.array(readlist)
+
+    return extracted
+
+
+if __name__ in "__main__":
+    with open(sys.argv[1], "r") as o:
+        obj = o.read()
+
+    v_ = re.findall("v\ .*", obj)
+    t_ = re.findall("vt\ .*", obj)
+    f_ = re.findall("f\ .*", obj)
+    vertices = extract(v_, numeric=True)
+    textures = extract(t_, numeric=True)
+    faces = extract(f_)
+
+    print(type(vertices), type(textures), type(faces))
+
+    # option 1: write all texture lines as vertex lines
