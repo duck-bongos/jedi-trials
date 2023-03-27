10c4146cfcc2f599728fd14811155c19b0c4f96b
Dan Billmann <billmannd@gmail.com>
2023-03-27 15:58:15 -0400

    created stub of file to convert mapped object to disc

---
+++ visualization/disc.py
@@ -0,0 +1,17 @@
+"""A module to read in an .obj file and write the textures to an image
+
+	Author: Dan Billmann
+
+"""
+import re
+import sys
+
+import numpy as np
+
+
+
+if __name__ in '__main__':
+	with open(sys.argv[1], 'r') as o:
+		obj = o.read()
+
+	re.findall()
